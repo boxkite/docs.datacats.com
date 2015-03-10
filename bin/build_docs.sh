05760831ecc912950d2ba89f13129e31ad862d6c
Ian Ward <ian@excess.org>
2015-03-10 12:57:24 -0400

    add build script

---
+++ bin/build_docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cd "`dirname $0`/../../datacats"
+rm -r build/sphinx || true
+python setup.py build_sphinx
+
+cd ../docs.datacats.com
+git fetch origin gh-pages
+git merge --ff-only origin/gh-pages
+git rm `git ls-files`
+git checkout HEAD CNAME
+git checkout HEAD .nojekyll
+git checkout HEAD bin
+cp -r ../datacats/build/sphinx/html/. .
+git add `find ../datacats/build/sphinx/html | cut -c 31-`
+git status
